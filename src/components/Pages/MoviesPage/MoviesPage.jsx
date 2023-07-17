a8b46571bdb8c3b5369b21cda67416ba6b8ed9c5
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2023-07-17 22:21:33 +0300

    add search component

--- src/components/Pages/MoviesPage/MoviesPage.jsx
+++ src/components/Pages/MoviesPage/MoviesPage.jsx
@@ -1,6 +1,20 @@
+import { Wrapper, Input, Icon } from './MoviesPage.styled';
 
 
-export const MoviesPage = () => {
+// export const MoviesPage = () => {
 
     
+// };
+
+export const MoviesPage = ({ value, onChange }) => {
+  return (
+    <Wrapper>
+      <Icon />
+      <Input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </Wrapper>
+  );
 };
--- src/components/Pages/MoviesPage/MoviesPage.styled.js
+++ src/components/Pages/MoviesPage/MoviesPage.styled.js
@@ -1,7 +1,23 @@
-// .Container {
-//     max-width: 1440px;
-//     margin-left: auto;
-//     margin-right: auto;
-//     padding-left: 15px;
-//     padding-right: 15px;
-// }
+import styled from "styled-components";
+import { HiSearch } from "react-icons/hi";
+
+export const Wrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
+  position: relative;
+  margin-bottom: 16px;
+  text-transform: uppercase;
+`;
+
+export const Input = styled.input`
+  padding: 8px 32px 8px 8px;
+  border-radius: 4px;
+  font: inherit;
+`;
+
+export const Icon = styled(HiSearch)`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 6px;
+`;
