d5810415c5b0e4fba8de612503af5027aa059d3a
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2023-08-07 21:15:32 +0300

    fix base name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
      
-  <BrowserRouter basename='react-hw-05-movies'>
+  <BrowserRouter basename='/react-hw-05-movies'>
       <App />
     
     </BrowserRouter>
